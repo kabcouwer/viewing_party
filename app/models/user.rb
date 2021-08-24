6bfd2fa42163b47d0f4e8b47a6b9d82be355bc50
JoannaCoPo <jococopo@gmail.com>
2021-08-24 14:31:57 -0600

    Add and test model relationships and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :friendships, dependent: :destroy
-  has_many :parties, foreign_key: :party_host_id, dependent: :destroy #class_name: "Friendship"
+  has_many :parties, foreign_key: :party_host_id, dependent: :destroy 
   has_many :attendees, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
