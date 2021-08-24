a76723ea96f4290b61525ffa3adf0174036d2612
JoannaCoPo <jococopo@gmail.com>
2021-08-24 14:12:28 -0600

    Add model relationships and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :friendships, dependent: :destroy
+  has_many :parties, foreign_key: :party_host_id, dependent: :destroy #class_name: "Friendship"
+  has_many :attendees, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
+
+  has_secure_password
 end
