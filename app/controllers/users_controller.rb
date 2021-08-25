b129ef88eac1dd2376e17c4a5be64b61a3b06db3
JoannaCoPo <jococopo@gmail.com>
2021-08-24 19:34:15 -0600

    Add user regsitration functionality.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
 
+  def create
+    user = user_params
+    user[:username] = user[:username].downcase
+    new_user = User.create(user)
+    flash[:success] = "Weclome, #{new_user.username}"
+    redirect_to dashboard_path
   end
 
   def login
     redirect_to root_path
+  end
+
+  def dashboard
+    
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :username, :password, :password_confirmation)
   end
 end
