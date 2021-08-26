d56b1cb0267f53769b2b852c040e3bf517fdf14e
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-08-26 15:45:17 -0600

    Refactor top_rated_movies method

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,13 +1,12 @@
 class MovieService
   def self.conn
     Faraday.new(url: 'https://api.themoviedb.org') do |req|
-      req.params['api_key'] = ENV['api_key']
+      req.params['api_key'] = ENV['movie_api_key']
     end
   end
 
   def self.top_rated_movies
-    response = conn.get("/3/movie/top_rated?api_key=#{ENV['api_key']}")
+    response = conn.get("/3/movie/top_rated")
     body = JSON.parse(response.body, symbolize_name: true)
-    binding.pry
   end
 end
