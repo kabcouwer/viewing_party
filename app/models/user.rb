84a5c89e5b403f4da656b42e2903d907fd3d3814
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-08-25 19:06:54 -0600

    Update convention to validations first, then relationships and bcrypt magic - no password validation needed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
-  has_many :friendships, dependent: :destroy
-  has_many :parties, foreign_key: :party_host_id, dependent: :destroy 
-  has_many :attendees, dependent: :destroy
-
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
-  validates :password, presence: true
+  # is validates password behind the scenes magic
+  # for password + password confirmation?
+
+  has_many :friendships, dependent: :destroy
+  has_many :parties, foreign_key: :party_host_id, dependent: :destroy
+  has_many :attendees, dependent: :destroy
 
   has_secure_password
 end
