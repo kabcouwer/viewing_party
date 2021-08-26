652485d007128d64f3590b1baf0916c736f65c0e
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-08-25 19:08:48 -0600

    Update happy path describe block

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -5,29 +5,24 @@ RSpec.describe 'Registration Page' do
     visit registration_path
   end
 
-  it 'has form to register new user' do
-    username = 'DirtyGertie03'
-    email = 'DirtyGertie03@doggo.com'
-    password = 'RainbowRoll4lyfe'
+  describe 'happy path' do
+    it 'has form to register new user' do
+      username = 'DirtyGertie03'
+      email = 'DirtyGertie03@doggo.com'
+      password = 'RainbowRoll4lyfe'
 
-    fill_in 'user[username]', with: username
-    fill_in 'user[email]', with: email
-    fill_in 'user[password]', with: password
-    fill_in 'user[password_confirmation]', with: password
+      fill_in 'user[username]', with: username
+      fill_in 'user[email]', with: email
+      fill_in 'user[password]', with: password
+      fill_in 'user[password_confirmation]', with: password
 
-    click_on 'Register'
+      click_on 'Register'
 
-    expect(current_path).to eq(dashboard_path)
-    # save_and_open_page
-    expect(page).to have_content("Weclome, #{username.downcase}")
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Welcome, #{username.downcase}")
+      expect(page).to have_no_content('Welcome to Viewing Party!')
+      expect(page).to have_no_button('Log In')
+      expect(page).to have_no_link('New to viewing party? Register here!')
+    end
   end
 end
-
-# When a user visits the '/registration' path they should see a form to register.
-# The form should include:
-#
-#  Email
-#  Password
-#  Password Confirmation
-#  Register Button
-# Once the user registers they should be logged in and redirected to the dashboard page
