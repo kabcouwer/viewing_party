da046b7125897d5483ac36e04754cf644b1fccdd
JoannaCoPo <jococopo@gmail.com>
2021-08-26 23:33:12 -0600

    Add top rated movies method.

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,12 +1,23 @@
 class MovieService
+  #ideally refactor at some point
+  def self.top_rated_movies
+    page_1_response = conn.get("/3/movie/top_rated?&page=1")
+    page_2_response = conn.get("/3/movie/top_rated?&page=2")
+    # moving this to a helper method to user with various methods/endpoints
+    # body = JSON.parse(response.body, symbolize_name: true)
+    page_1_body = parse_json(page_1_response)
+    page_2_body = parse_json(page_2_response)
+
+    page_1_body[:results] + page_2_body[:results]
+  end
+
   def self.conn
     Faraday.new(url: 'https://api.themoviedb.org') do |req|
       req.params['api_key'] = ENV['movie_api_key']
     end
   end
 
-  def self.top_rated_movies
-    response = conn.get("/3/movie/top_rated")
-    body = JSON.parse(response.body, symbolize_name: true)
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
