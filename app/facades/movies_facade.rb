3d454aee12fef4350db2210021d2ff9338ef8cc1
JoannaCoPo <jococopo@gmail.com>
2021-08-27 15:52:16 -0600

    Remove unnecessary variable

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -1,6 +1,6 @@
 class MoviesFacade
   def self.top_rated #returns array of movie objects
     response_data = MovieService.top_rated_movies
-    x = response_data.map { |data| Movie.new(data) }
+    response_data.map { |data| Movie.new(data) }
   end
 end
