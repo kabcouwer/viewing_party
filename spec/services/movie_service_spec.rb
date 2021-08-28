d49413afda901c555be1dd970e93adc0c30fa48f
JoannaCoPo <jococopo@gmail.com>
2021-08-28 16:53:37 -0600

    Make assertions more rubust.

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 describe MovieService do
-  describe "#top rated movies" do
+  describe '#top rated movies' do
     it 'displays data for top 40 rated movies' do
       top_rated_movies_stubs
 
-      response = MovieService.top_rated_movies.first
-      title = "Evangelion: 3.0+1.0 Thrice Upon a Time"
+      response = MovieService.top_rated_movies
+      title = 'Evangelion: 3.0+1.0 Thrice Upon a Time'
 
-      expect(response).to be_a(Hash)
-      expect(response[:title]).to eq(title)
-      expect(response[:vote_average]).to eq(8.8)
+      expect(response).to be_an(Array)
+      expect(response.first).to be_a(Hash)
+      expect(response.first[:title]).to eq(title)
+      expect(response.first[:vote_average]).to eq(8.8)
+      expect(response.count).to eq(40)
     end
   end
 end
+
+#confirm whether or not we need to test the helper methods
