e602ab7288defd0487a89425eef931609e86a506
JoannaCoPo <jococopo@gmail.com>
2021-08-28 14:00:07 -0600

    Move stub_requests to spec_helper.

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -7,27 +7,7 @@ RSpec.describe "Movies Index Page" do
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-      response_body_1 = File.read('spec/fixtures/top_rated_page_1.json')
-
-      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['movie_api_key']}&page=1").
-         with(
-           headers: {
-          'Accept'=>'*/*',
-          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'User-Agent'=>'Faraday v1.7.0'
-           }).
-         to_return(status: 200, body: response_body_1, headers: {})
-
-      response_body_2 = File.read('spec/fixtures/top_rated_page_2.json')
-
-      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['movie_api_key']}&page=2").
-         with(
-           headers: {
-          'Accept'=>'*/*',
-          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'User-Agent'=>'Faraday v1.7.0'
-           }).
-         to_return(status: 200, body: response_body_2, headers: {})
+      top_rated_movies_stubs #hidden in spec_helper :)
 
       visit movies_path
     end
