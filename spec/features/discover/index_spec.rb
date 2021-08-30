a9742a24f5dfad9ebef99cfd83e14c1a338c6556
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-08-29 18:52:11 -0600

    Implement vcr in tests

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -3,43 +3,34 @@ require 'rails_helper'
 RSpec.describe "Discover Page" do
   describe "Top rated movies and search by title" do
     before :each do
-      WebMock.allow_net_connect! #not sure this is ideal
-
       @user = create(:user)
+
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
       visit discover_path
     end
 
     it 'displays button to find top rated 40 movies' do
-      expect(page).to have_button('Find Top Rated Movies')
+      VCR.use_cassette('top_rated_movies') do
 
-      click_on "Find Top Rated Movies"
+        expect(page).to have_button('Find Top Rated Movies')
 
-      expect(current_path).to eq(movies_path)
+        click_on "Find Top Rated Movies"
+
+        expect(current_path).to eq(movies_path)
+      end
     end
 
     it 'redirects to the movies index when a title is submitted to the movie search' do
-      expect(page).to have_content('Search by movie title:')
+      VCR.use_cassette('specific_movie_search') do
+        expect(page).to have_content('Search by movie title:')
 
-      fill_in :movie_search, with: 'Kim and Jo Dominate Mod 3'
+        fill_in :movie_search, with: 'Codependent Lesbian Space Alien Seeks Same'
 
-      click_button('Find Movies')
+        click_button('Find Movies')
 
-      expect(current_path).to eq(movies_path)
+        expect(current_path).to eq(movies_path)
+      end
     end
   end
 end
-#
-# As an authenticated user,
-# When I visit the '/discover' path
-# I should see
-#
-#  Button to Discover top 40 movies
-# Details When the user clicks on the top 40 button they should be taken to the movies page.
-#
-#  A text field to enter keyword(s) to search by movie title
-#  A Button to Search by Movie Title
-# Details When the user clicks on the Search button they should be taken to the movies page
-#
-# The movies will be retrieved by consuming The MovieDB API
