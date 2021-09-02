fa370e37dfb839fc6e617524beb4f158662ae556
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-09-01 20:30:36 -0600

    Fix current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   private
